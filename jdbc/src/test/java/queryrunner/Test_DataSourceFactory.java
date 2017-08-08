50b69aafeabd785cf1167fc21a8b373d99133818
mfdyyge@qq.com <mfdyyge@qq.com>
2017-08-08 21:04:21 +0800

    编译命令:2017年8月8日20:37:36

--- jdbc/src/test/java/queryrunner/Test_DataSourceFactory.java
+++ jdbc/src/test/java/queryrunner/Test_DataSourceFactory.java
@@ -3,10 +3,15 @@ package queryrunner;
 import com.zl.jdbc.DataSource.DataSouceFactory;
 import com.zl.jdbc.apche.dbutils.dao.QueryRunnerDao;
 
+import com.zl.jdbc.page.OfferType;
+import com.zl.jdbc.page.Page;
+import com.zl.jdbc.page.QueryRemote;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by 钢背猪☣ on 2017-8-7 0007.
@@ -32,6 +37,31 @@ public class Test_DataSourceFactory
 
         Object result[]=QueryRunnerDao.getFirstRowArray(connection,sql,params);
         System.out.println(Arrays.asList(result));
+
+    }
+
+    @Test
+    public void page_test(){
+
+            Page page = new Page();
+            page.setPageNum(1);
+            page.setPageSize(4);
+            Object[] params = new Object[]{1} ;
+
+            List<OfferType> offerTypes = new ArrayList<OfferType>();
+            String sql = "select * from offer_type where offer_type > ?";
+            List<Object> list = QueryRemote.query(sql, page, params, OfferType.class);
+
+            OfferType offerType = null;
+            for (int i = 0; i < list.size(); i++) {
+                offerType = new OfferType();
+                offerType = (OfferType) list.get(i);
+                System.out.println(offerType.toString());
+                offerTypes.add(offerType);
+            }
+
+            System.out.println();
+            System.out.println("共"+page.getTotalSize()+"条记录，每页大小"+page.getPageSize()+"，当前第"+page.getPageNum()+"页，查询到 "+list.size()+" 条记录。。。");
 
     }
 }
