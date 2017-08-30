d20c1a3a90a286a345ca24d7be002471d5d46aa0
mfdyyge <mfdyyge@qq.com>
2017-08-30 19:11:48 +0800

    JDBCTable.java 获取表字段对应的JAVA类型

---
+++ jdbc/src/test/java/DataSource/JdbcSchema.java
@@ -0,0 +1,13 @@
+package DataSource;
+
+/**
+ * Created by 钢背猪☣ on 2017-8-30 0030.
+ *
+ * @ClassName: DataSource
+ * @Description: 描述:474752515@qq.com
+ * @author: 钢背猪☣
+ * @date: 2017-8-30 0030
+ */
+public class JdbcSchema
+{
+}
